7bcacda85f504e355102b8695d5f35754b742ea2
hugo.coutinho <hugo.coutinho@dxtcps010119.local>
2021-08-17 17:27:30 -0300

    adding love colors

--- packages/widgets/lib/constant/constant.dart
+++ packages/widgets/lib/constant/constant.dart
@@ -10,5 +10,8 @@ String defaultBackgroundImageAsset = baseAsset + "default_background_image.png";
 // COLORS
 Color faithBackgroundColor = Colors.yellow[50];
 Color inspirationalBackgroundColor = Colors.blue[50];
+Color loveBackgroundColor = Colors.red[200];
+
 Color faithBarrierColor = Colors.yellow[100];
+Color loveBarrierColor = Colors.red[300];
 Color inspirationalBarrierColor = Colors.blue[100];
